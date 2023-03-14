a9e7a7b341cf8dc2ff8edd280eaf139774d3889e
FrancoIzder <franco.erramouspe@izder.uy>
2023-03-14 11:30:30 -0300

    angular-sprint-0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { UsersComponent } from './users/users.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SideBarComponent,
+    UsersComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
